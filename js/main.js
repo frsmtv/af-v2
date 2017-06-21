e0c831db9947df1f101d4a917829bbae46c9be88
frsmtv <fsimitchiev@gmail.com>
2017-06-22 00:48:29 +0200

    uyyu

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
 $(document).ready(function () {
 
   // ANIMATIONS ON SCROLL START
-    //   var scroll_start = 0;
-    //   var startchange = $('body');
-    //   var offset = startchange.offset();
-    //   $(document).scroll(function() {
-    //       scroll_start = $(this).scrollTop();
-    //       if(scroll_start > offset.top) {
-    //         $('h1 span').css('color', 'white');
-    //
-    //       } else {
-    //         $('h1 span').css('color', '');
-    //   });
-    // });  
+      var scroll_start = 0;
+      var startchange = $('body');
+      var offset = startchange.offset();
+      $(document).scroll(function() {
+          scroll_start = $(this).scrollTop();
+          if(scroll_start > offset.top) {
+            $('h1').css('margin-top', '0');
+            $('.toggle-button').css('top', '0');
+
+          } else {
+            $('h1').css('margin-top', '');
+            $('.toggle-button').css('top', '');
+      }
+    });
 
 });
