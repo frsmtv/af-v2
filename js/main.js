cde67d63582d3e64bb89866478b7b7bdfbe86a36
frsmtv <fsimitchiev@gmail.com>
2017-06-30 17:00:45 +0200

    ioio

--- js/main.js
+++ js/main.js
@@ -11,14 +11,14 @@ $(document).ready(function () {
             $('h1').css('margin-top', '6px');
             $('span.title').css('opacity', '0');
             $('.toggle-button').css('top', '10px');
-            $('#intro i').fadeOut();
+            $('i').fadeOut();
 
           } else {
             // $('h1').css('opacity', '');
             $('h1').css('margin-top', '');
             $('span.title').css('opacity', '');
             $('.toggle-button').css('top', '');
-            $('#intro i').fadeIn();
+            $('i').fadeIn();
       }
     });
 
@@ -84,5 +84,33 @@ $(document).ready(function () {
     }, 'slow');
       $('footer').show();
     })
+
+    $('#portfolio-link').click(function(){
+      $('h1').css('opacity', '1');
+      $('#intro').hide();
+      $('#info').hide();
+      $('#savoir-faire').hide();
+      $('#portfolio').show();
+      $('html, body').animate({
+        scrollTop: $('#portfolio').offset().top - 20
+    }, 'slow');
+      $('#contact').hide();
+      $('footer').hide();
+    })
+
+    $('#contact-link').click(function(){
+      $('h1').css('opacity', '1');
+      $('#intro').hide();
+      $('#info').hide();
+      $('#savoir-faire').hide();
+      $('#portfolio').hide();
+      $('#contact').show();
+      $('html, body').animate({
+        scrollTop: $('#contact').offset().top - 20
+    }, 'slow');
+      $('footer').show();
+    })
+
+
 
 });
