e07e452da66c1070be6710996d535662d5186359
acheynet <acheynet@gmail.com>
2017-07-24 12:22:01 +0200

    Moved the content of overlay.js to main.js

--- js/main.js
+++ js/main.js
@@ -10,6 +10,22 @@ $(document).ready(function() {
 		fitToSection: false,
 		fitToSectionDelay: 100,
 	});
+	// OVERLAY
+	$('#toggle').click(function() {
+		$(this).toggleClass('toggle-active');
+		$('#overlay').toggleClass('nav-active');
+		$('#overlay li').toggleClass('slit-in-horizontal');
+		$('#contact-link').toggleClass('hide');
+		$('main').toggleClass('slide-out-blurred-top');
+	});
+	// HIDE MENU ON CLICK ON A LINK
+	$('#overlay li').click(function(){
+		$('#overlay').toggleClass('nav-active');
+		$('#toggle').toggleClass('toggle-active');
+		$('#contact-link').toggleClass('hide');
+		$('#overlay li').toggleClass('slit-in-horizontal');
+		$('main').toggleClass('slide-out-blurred-top');
+	});
 	// ANIMATIONS ON SCROLL START
 	// UI ANIMATION
 	$('main').addClass('animated fadeIn');
