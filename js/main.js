0e8b4ae2c080a841010c98a6b0e301702f54b2fc
frsmtv <fsimitchiev@gmail.com>
2017-07-29 10:06:12 +0200

    worked on blog responsive, corrected main.js, added preloader

--- js/main.js
+++ js/main.js
@@ -9,12 +9,14 @@ $(document).ready(function () {
           if(scroll_start > offset.top) {
             $('h1').css('margin-top', '2px');
             $('.toggle-button').css('top', '10px');
-            $('#contact-link').css('top', '20px')
+            $('#contact-link').css('top', '20px');
+            $('#h2-blog').css('top', '50px');
 
           } else {
             $('h1').css('margin-top', '');
             $('.toggle-button').css('top', '');
-            $('#contact-link').css('top', '')
+            $('#contact-link').css('top', '');
+            $('#h2-blog').css('top', '');
       }
     });
 
@@ -22,10 +24,10 @@ $(document).ready(function () {
     $('body').addClass('animated fadeIn');
 
     // ACTIVATE POINTER EVENTS ON CLICK ON MAP
-      $('#map').css('pointer-events', 'none');
+    $('#map').css('pointer-events', 'none');
 
-      $('.map-wrapper').click(function() {
-        $('#map').css('pointer-events', 'auto');
-      });
+    $('.map-wrapper').click(function() {
+      $('#map').css('pointer-events', 'auto');
+    });
 
 });
